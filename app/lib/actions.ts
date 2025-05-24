79e3b1cbea4d5fb7dab46428a49f6d875f96d21c
ChrisDavis <vito186cm@gmail.com>
2025-05-24 17:33:02 +1000

    add authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,9 +5,30 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
  
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
+    }
+  }
  
 const FormSchema = z.object({
     id: z.string(),
@@ -100,7 +121,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
         WHERE id = ${id}
       `;
     } catch (error) {
-      return { message: 'Database Error: Failed to Update Invoice.' };
+      return { message: 'Database Error: Failed to Update Invoice.', error };
     }
    
     revalidatePath('/dashboard/invoices');
