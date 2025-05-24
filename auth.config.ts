1d45a281c25cf9b4bea3718075d7b7380fd3bc3b
ChrisDavis <vito186cm@gmail.com>
2025-05-24 17:47:21 +1000

    fix login

--- auth.config.ts
+++ auth.config.ts
@@ -6,6 +6,34 @@ export const authConfig = {
   pages: {
     signIn: '/login',
   },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      // biome-ignore lint/style/noUselessElse: <explanation>
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+      return true;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id as string;
+        session.user.email = token.email as string;
+      }
+      return session;
+    }
+  },
   providers: [
     Credentials({
       async authorize(credentials) {
@@ -24,18 +52,4 @@ export const authConfig = {
       },
     }),
   ],
-  callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      // biome-ignore lint/style/noUselessElse: <explanation>
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
-      }
-      return true;
-    },
-  },
 } satisfies NextAuthConfig;
